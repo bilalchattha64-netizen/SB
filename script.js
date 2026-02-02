5811cf6de00e1ba9c781a14a0aed6f1a561db95c
Bilal Pervaiz Chatta <bilalchattha64@gmail.com>
2026-02-02 15:50:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const noBtn = document.getElementById("noBtn");
 const message = document.getElementById("message");
 
 yesBtn.onclick = () => {
-  message.innerText = "Yay! You made my day ❤️✨";
+  message.innerText = "Yay! Celebration time! 🎉❤️";
 };
 
 noBtn.onmouseover = () => {
   const x = Math.random() * 200 - 100;
   const y = Math.random() * 200 - 100;
-
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 };
